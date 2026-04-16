b72e601dd54342f79322e76db106e473ba3aed68
franciscootrejo <ftrejoo@centro.edu.mx>
2026-04-16 13:52:54 -0600

    cambio

--- js/botones.js
+++ js/botones.js
@@ -1,29 +1,33 @@
+let title = document.querySelector("h1");
 let boton1 = document.getElementById("btn-1");
 let boton2 = document.getElementById("btn-2");
 let boton3 = document.getElementById("btn-3");
-
-boton1.addEventListener("mouseenter", function() {
+boton1.addEventListener("click", function() {
+    title.innerText = "Botón 1";
     bgColor("red");
 });
-boton1.addEventListener("mouseleave", function() {
+boton1.addEventListener("mouseenter", function() {
+    title.innerText = "Botón";
     bgColor("blue");
 });
 
-boton3.addEventListener("mouseenter", function() {
+boton3.addEventListener("click", function() {
+    title.innerText = "Botón 3";
     bgColor("pink");
 });
-boton3.addEventListener("mouseleave", function() {
+boton3.addEventListener("mouseenter", function() {
+    title.innerText = "Botón";
     bgColor("aquamarine");
 });
 
-boton2.addEventListener("mouseenter", mouseEnter);
-boton2.addEventListener("mouseleave", mouseLeave);
-function mouseEnter() {
-    bgColor("lightblue");
-}
-function mouseLeave() {
-    bgColor("white");
-}
+boton2.addEventListener("click", function() {
+    title.innerText = "Botón 2";
+    bgColor("pink");
+});
+boton2.addEventListener("mouseenter", function() {
+    title.innerText = "Botón";
+    bgColor("aquamarine");
+});
 function bgColor(color) {
     document.body.style.backgroundColor = color;
 }    
