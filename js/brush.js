2f7e8aa220a46076ec084f7ca7ccdcd1cd9b6e86
franciscootrejo <ftrejoo@centro.edu.mx>
2026-04-23 13:56:06 -0600

    brush

--- js/brush.js
+++ js/brush.js
@@ -1,13 +1,35 @@
-let color = "black";
-let size stroke_weight = 1;
-
+let color = "aquamarine";
+let stroke_weight = 5;
+let coordinates = [];
+let colorPicker;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  colorPicker = createColorPicker(color);
+  colorPicker.position(20, 20); 
 }
 
 function draw() {
-  
+    background(255);
+    color = colorPicker.value();
+    stroke(color);
+    strokeWeight(stroke_weight);
+    noFill(0);
+    beginShape();
+   /* vertex(0, 100);
+    vertex(100, 100);
+    vertex(0, 100);*/
+    for (let i = 0; i < coordinates.length; i++) {
+        let coord = coordinates[i];
+        vertex(coord.x, coord.y);
+    }
+    endShape();
+
 }   
 function mousePressed() {
+    coordinates.push({x: mouseX, y: mouseY});
+}
+
+function mouseDragged() {
+    coordinates.push({x: mouseX, y: mouseY});
 }
